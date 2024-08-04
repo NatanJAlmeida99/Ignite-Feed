398b091c2de0758624247be6bbd54849a8755806
Natan Almeida <natanalmeida158@hotmail.com>
2024-08-04 14:58:26 -0300

    validando form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,7 +33,8 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText('');
     }
 
-    function handelNewCommentChange() {
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);   
     }
     
@@ -50,6 +51,8 @@ export function Post({ author, publishedAt, content }) {
         })
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
     
     return (
         <article className={styles.post}>
@@ -84,12 +87,14 @@ export function Post({ author, publishedAt, content }) {
                 name='comment'
                 placeholder='Deixe um comentário' 
                 value={newCommentText}
-                onChange={handelNewCommentChange}
+                onChange={handleNewCommentChange}
                 onInvalid={handleNewCommentInvalid}
                 required
                 />
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
             
